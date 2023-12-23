420b4ac2f9a9dc1e155c75af88cc2385daa86a9d
cnotgate <49557224+cnotgate@users.noreply.github.com>
2023-12-24 05:54:32 +0700

    Built web
    Branch: landing-page
    Commit: 25aed9c0e3d3edd5a88d064f0158536897a04ea4
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "ec548bfadac58d9092712323c512e678
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "baa06984b8a78dd5e653d05deb7724db",
-"/": "baa06984b8a78dd5e653d05deb7724db",
-"main.dart.js": "8d8dcd400b17dcd739ee41cb29d5d4f3",
+"index.html": "a29ad0e39d9390a73a216216f4a9b556",
+"/": "a29ad0e39d9390a73a216216f4a9b556",
+"main.dart.js": "b0e4f0337bb6749298184e0974e3a927",
 "manifest.json": "6079311bd9d2860f5370916ea7a1796d",
 "version.json": "a2ab88aa555837f4a26c44f5b9a351b9"};
 // The application shell files that are downloaded before a service worker can
